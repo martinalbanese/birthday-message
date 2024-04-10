f350f7975305f9d3e30703a1213d3a87ce58df23
martinalbanese <albanesemartinax@gmail.com>
2024-04-10 14:52:53 +0200

    correzione bug

--- js/dom-manipulator/index.js
+++ js/dom-manipulator/index.js
@@ -1,4 +1,3 @@
 export { blowCandle } from './blowCandle.js';
-export { createBirthdayCake } from './createCake.js';
-export { startFireworks } from './createFireworks.js';
 export { showBirthdayMessage } from './showMessage.js';
+export { startFireworks } from './createFireworks.js'
